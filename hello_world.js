b1f34e31acc958c0dd98b3622e0de1450c171bbb
S.P.Balamurugan <spbalamuruga@gmail.com>
2017-06-24 09:34:56 +0530

    Update hello_world.js

--- hello_world.js
+++ hello_world.js
@@ -1,7 +1,14 @@
-var http = require("http")
-function request_handler(req,res){
-	var body_text = "hello from guvi";
-	res.end(body_text);
-}
-var svr = http.createserver(request_handler);
-svr.listen(8080);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World GUVI from St.Joseph\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
